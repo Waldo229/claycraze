105afc12d6daf444c83d9b0f666a4377d9892c40
jim <jim@claycraze.com>
2026-05-07 13:34:05 -0400

    Render Deploy to SG-staging

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,11 @@
 set -e
 
 echo "Writing SSH key..."
-
 printf '%s' "$SG_PRIVATE_KEY_B64" | base64 --decode > /tmp/render-sg
 sed -i 's/\r$//' /tmp/render-sg
 chmod 600 /tmp/render-sg
 
 echo "Testing SSH connection..."
-
 ssh \
   -i /tmp/render-sg \
   -p "$SG_PORT" \
@@ -17,13 +15,16 @@ ssh \
   "$SG_USER@$SG_HOST" \
   "echo SSH SUCCESS"
 
-echo "Copying public files to SiteGround staging..."
+echo "Deploying repo files to SiteGround staging public_html..."
 
-scp \
-  -i /tmp/render-sg \
-  -P "$SG_PORT" \
-  -o StrictHostKeyChecking=no \
-  -r public/* \
-  "$SG_USER@$SG_HOST:~/staging.claycraze.com/"
+rsync -avz --delete \
+  --exclude ".git" \
+  --exclude "node_modules" \
+  --exclude ".env" \
+  --exclude "*.db" \
+  --exclude "deploy.sh" \
+  -e "ssh -i /tmp/render-sg -p $SG_PORT -o StrictHostKeyChecking=no" \
+  ./ \
+  "$SG_USER@$SG_HOST:~/staging.claycraze.com/public_html/"
 
 echo "SiteGround staging deploy complete."
